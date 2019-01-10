3df7566dfb04b9142d3660e85dcf557bcc6cf593
AlexandraChiritaACS <46438366+AlexandraChiritaACS@users.noreply.github.com>
2019-01-10 19:23:38 +0200

    Update CustomerAddress.cs

--- WorkingWithAzureDB/WorkingWithAzureDB/CustomerAddress.cs
+++ WorkingWithAzureDB/WorkingWithAzureDB/CustomerAddress.cs
@@ -11,15 +11,16 @@ namespace WorkingWithAzureDB
 {
     using System;
     using System.Collections.Generic;
-    
+    // Class to define the address of a Customer linked to address
     public partial class CustomerAddress
     {
+        // Main parameters of a customer in DB
         public int CustomerID { get; set; }
         public int AddressID { get; set; }
         public string AddressType { get; set; }
         public System.Guid rowguid { get; set; }
         public System.DateTime ModifiedDate { get; set; }
-    
+        // Address of a customer
         public virtual Address Address { get; set; }
         public virtual Customer Customer { get; set; }
     }
